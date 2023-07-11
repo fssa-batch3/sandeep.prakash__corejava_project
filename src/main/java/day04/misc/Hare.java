417750692c54ebbbb6cbc399dda4625fdd94888d
Sandeepsan97 <116252886+Sandeepsan97@users.noreply.github.com>
2023-07-11 18:22:18 +0530

    day 04 commit
    day 04 commit

---
+++ src/main/java/day04/misc/Hare.java
@@ -0,0 +1,15 @@
+package day04.misc;
+
+public class Hare {
+	public void init() {
+
+		System.out.println("init-");
+
+		}
+
+		protected void race() {
+
+		System.out.println("hare-");
+
+		}
+}
---
+++ src/main/java/day04/practice/PolygonTest.java
@@ -0,0 +1,24 @@
+package day04.practice;
+
+public class PolygonTest {
+	public static void main(String[] args) {
+		Polygon rec;
+		
+		try {
+			
+			rec = new Rectangle(1,1);
+	
+			rec.calculateArea();
+			rec.circumference();
+			
+			RightAngledTriangle ratt = new RightAngledTriangle(20.0, 10.0);
+			ratt.calculateArea();
+			ratt.circumference();
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
