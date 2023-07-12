8d26f67a1eabd7ad86af4bb054ef9d31bbecf414
Sandeepsan97 <116252886+Sandeepsan97@users.noreply.github.com>
2023-07-12 19:38:29 +0530

    day05
    day05

--- src/main/java/day04/practice/RightAngledTriangle.java
+++ src/main/java/day04/practice/RightAngledTriangle.java
@@ -1,14 +1,13 @@
 package day04.practice;
 
-public class RightAngledTriangle extends Polygon{
+public class RightAngledTriangle extends Polygon {
 
-	
 	protected double base;
 	protected double height;
-	
+
 	public RightAngledTriangle(double length, double breadth) throws Exception {
 		super(3);
-		if(base <= 0 || height <= 0) {
+		if (base <= 0 || height <= 0) {
 			throw new Exception("Invalid dimensions for a Triangle");
 		}
 		this.base = base;
@@ -18,17 +17,17 @@ public class RightAngledTriangle extends Polygon{
 	@Override
 	public double calculateArea() {
 		// TODO Auto-generated method stub
-		return 1.0/2.0 *(this.base * this.height);
+		return 1.0 / 2.0 * (this.base * this.height);
 	}
-	
+
 	public int getNumberOfSides() {
 		return super.numberOfSides;
 	}
 
 	@Override
 	public double circumference() {
-		double c=Math.round(Math.sqrt(Math.pow(base,2)+Math.pow(height, 2)));
-		double circumference=base+height+c;
+		double c = Math.round(Math.sqrt(Math.pow(base, 2) + Math.pow(height, 2)));
+		double circumference = base + height + c;
 		return circumference;
 	}
 
