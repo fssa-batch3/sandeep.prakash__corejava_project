c504bfc93660d40a5a47983d683569008f901374
Sandeepsan97 <116252886+Sandeepsan97@users.noreply.github.com>
2023-07-15 11:42:43 +0530

    testcase
    testcase

--- src/main/java/day07/practice/TaskDuplicate.java
+++ src/main/java/day07/practice/TaskDuplicate.java
@@ -7,6 +7,9 @@ import java.time.LocalDate;
  */
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TaskDuplicate {
 
@@ -38,16 +41,67 @@ public class TaskDuplicate {
 		this.deadline = deadline;
 	}
 
+	
 	public TaskDuplicate(int id, String name, LocalDate deadline) {
 		this.id = id;
 		this.name = name;
 		this.deadline = deadline;
 	}
 	
+	
+	public static boolean  validatetaskName (String name)throws IllegalArgumentException {
+		if(name==null || name.isEmpty()) {
+			throw new IllegalArgumentException("TaskName cannot be null or empty");
+		}
+		String nameregex = "^[a-zA-Z]{2,200}$";
+		Pattern pattern = Pattern.compile(nameregex);
+		Matcher matcher = pattern.matcher(name);
+		Boolean isMatch = matcher.matches();
+
+
+
+		 if (!isMatch) {
+			throw new IllegalArgumentException("The  task name should be  minimum 2 letters and maximum 200 letters");
+
+		}
+		 return true;
+
+		
+	}
+	public static boolean  validateTaskDate(LocalDate deadline)throws IllegalArgumentException {
+		LocalDate currentDate = LocalDate.now();
+		if(deadline==null) {
+			throw new IllegalArgumentException("Task date cannot be null");
+		}
+		if(deadline.isBefore(currentDate)) {
+			throw new IllegalArgumentException("Task date cannot be in the past.");
+			
+		}
+		return true;
+		
+	}
+	
     @Override
     public String toString() {
         return "TaskDuplicate [id=" + id + ", name=" + name + ", deadline=" + deadline + "]";
     }
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(deadline, id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TaskDuplicate other = (TaskDuplicate) obj;
+		return Objects.equals(deadline, other.deadline) && id == other.id && Objects.equals(name, other.name);
+	}
 
 	public static void main(String[] args) {
 
@@ -70,4 +124,20 @@ public class TaskDuplicate {
 		System.out.println(uniqueArr);
 
 	}
+	
+	public static boolean validateDuplicatemethod(ArrayList<TaskDuplicate> taskList) throws IllegalArgumentException{
+		
+		HashSet<String> taskSet = new HashSet<>();
+		for(TaskDuplicate task:taskList) {
+			   String taskKey = task.getName() + "_" + task.getDeadline();
+			   if(taskSet.contains(taskKey)) {
+				   throw new IllegalArgumentException("duplicate task found");
+			   }
+			   taskSet.add(taskKey);
+		
+		}
+		return true;
+	}
+	
+	
 }
